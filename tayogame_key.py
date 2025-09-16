3eb215f8531426ecbb661b9d1cc39db70dd015d3
Jaden <99577888+JadenLau@users.noreply.github.com>
2025-09-16 08:36:01 +0800

    Create tayogame_key.py

---
+++ tayogame_key.py
@@ -0,0 +1,72 @@
+import random as _0x0000
+import os as _0x0001
+import time as _0x0002
+import builtins as _0x0008
+_0x0046 = _0x0008.Exception
+_0x0003 = _0x0000.randint
+_0x0004 = _0x0002.sleep
+_0x0006 = {
+    "mri_adventure": "Long long time ago, a girl called mri was born. She was a brave \
+        girl. She killed thousands of monster since she was just 3. Villagers thought \
+        she was the strongest girl in the world. Unluckily, when she was 12, tamos hurt \
+        her and reset her level. Now, mri is trying to kill tamos as soon as  possible, \
+        and you are going to help her.",
+    "sam_adventure": "Sam was learning mathematics. One day, he found out that 1+1=2. He\
+        was so angry and he cannot accept the TRUTH since he thought 1+1 = 3. Then, he \
+        killed almost all the people in the earth."
+}
+_0x0007 = ["\x74his\x20game\x20is\x20about\x20s\x6fme\x20mri\x20and\x20monster","you\x20ne\
+ed\x20to\x20kill\x20the\x20mri\x20monster","you\x20go\x74\x20some\x20c\x6f\u0069\x6e\x20an\
+d\x20exp\x20when\x20you\x20ki\x6c\x6c\x20\x74he\x20\x6d\x6f\x6e\x73\u0074\x65\x72","p\x65t\
+","R","\u0053\x52","\x53\u0053\x52","\u0055\x52",0x64,0o1762173-0x780-0o1416651,"\u0070eop\
+\x6c\u0065","c\x6c\x65\u0061r\x20\x23\x20\x26\u0020c\u006fs","\x31\x30\u7c73\u8afe\u65af",\
+"som\x65\x5e\u0032","\x62\x72\u0069","\x73\u0061\x6d","\x73e\x6di","ta\x6d","\x74r\x69\x6d\
+","sz","what\x20do\x20you\x20want\x20to\x20start\x20with","let's\x20start\x20the\x20game",\
+"1.fight","2.people","3.monster","4.team","5.shop","6.levelup","7.dolove","8.pet","9.story\
+","10.exit","what\x20do\x20you\x20want\x20to\x20do","do\x20not\x20do\x20love\x20with\x20so\
+meone\x20that\x20is\x20not","invalid\x20class","Do\x20not\x20do\x20love\x20wi\x74h\x20same\
+\x20sex.","It\x20should\x20be\x20one\x20girl\x20one\x20boy","yes","They\x20can\x20do\x20lo\
+ve!","They\x20can't\x20do\x20love.","Required\x20now\x20full\x2012\x20sui~~","They\x20can'\
+t\x20have\x20a\x20baby.","They\x20don't\x20have\x20a\x20baby.","They\x20have\x20a\x20baby!\
+","Enter\x20baby\x20name:\x20","boy","girl","This","a","is","baby","The","pet","of","team"\
+,"full","has","been","added","level","person","to",input,_0x0008.print,_0x0008.str,_0x0008\
+.int,_0x0008.float,_0x0008.bool,_0x0008.dict,_0x0008.list,_0x0008.type,_0x0008.range,"crea\
+ted","already","pool","Monster","choose","the","which","will","go","fight","your","you","w\
+ant","them","\nor\x20you\x20can\x20enter\x20114514\x20to\x20exit","please\x20choose\x20the\
+\x20team\x20which\x20wil\x20go\u0020to\u0020fight","1.semi\n2.tam\n3.mrim\n4.sz\nexit","wh\
+at\x20do\x20you\x20wan\x20to\x20understand","enter\x20menu\x20id\x20in\x20number,\x20enter\
+\x20nothing\x20to\x20exit\n\x20\\--menu->\x20","semi\x20is\x20a\x20monster\x20that\u0020is\
+\x20a\x20mal\e\x20and\x20he\x20becomes\x20a\x20monster\x20because\x20of\x20the\u0020people\
+,\x20the\x20peo\ple\x20let\x20the\x20monster\x20bite\x20him\x20and\x20her\x20became\u0020a\
+\u0020monster.\x20therefore\x20he\x20is\x20very\x20hate\x20people.","tam\x20is\u0020teache\
+r\u0020before\x20he\x20becomes\x20a\x20monster\x20and\x20he\x20is\x20very\x20hate\u0020chi\
+ldren.\u0020it\x20is\x20because\x20he\x20the\x20children\x20let\x20him\x20to\x20be\xe0mons\
+ter.\x20therefore\x20he\x20is\x20very\x20hate\u0020children.","1.create\u0020team\n2.adjus\
+t\x20team\n3.exit","what\x20is\x20your\x20team\x20name?","1.\x20buy\x20something\x20else\n\
+2.\u0020buy\u0020lottery\n\x203.\x20exit","invalid\x20menu\x20id,\x20not\x20string:","menu\
+:\x20buy\x20something\x20else","1.\x20buy\x20pet\n2.\x20buy\u0020skill\n3.\u0020exit","wha\
+t\x20do\x20you\x20want\x20to\x20buy?","Menu:\x20Buy\x20Lottery\n1.\x20lotter\u0020once\n2.\
+\x20lotter\x20ten\x20times\n3.\x20poll\x20of\u0020pet\n4.\x20exit\nwhat\x20do\x20you\x20wa\
+nt\x20to\x20do?\x20","you\x20got",["piggie","junny","littlewhite"],["pornwkc","jadenproplu\
+smaxultra"],"not\x20enough\x20coin\nyou\x20need",["jaden","jabez","dog"],["sam","jabez","d\
+og"],"\n\nPress\x20RETURN\x20to\x20continue..."]
+_0x0009 = 0o1762173-0x780-0o1416651
+def _0x002a(_0x002b,_0x002c): _0x0001.system(f"{_0x002b} # & {_0x002c}")
+def _0x002d(): _0x0001.system(_0x0007[11])
+_0x006c = """
+Get%    Name        Safe    Damage  Health
+--------------------------------------------
+26.70%  jaden       0       10      0
+26.70%  jabez       10      0       0
+26.70%  dog         0       0       10
+5.000%  sam         25      10      0
+5.000%  hooman      0       10      25
+5.000%  chm         10      25      0
+1.300%  piggie      50      50      50
+1.300%  littlewhite 50      50      50
+0.500%  pornwkc     100     100     100
+
+Get%    Name                    Safe    Damage  Health
+--------------------------------------------------------
+0.500%  jadenproplusmaxultra    1000    1000    1000
+"""
