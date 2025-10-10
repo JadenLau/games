66eb970a996d9199b45764947c71ed4bebbca9f2
Jaden <99577888+JadenLau@users.noreply.github.com>
2025-10-10 08:54:47 +0800

    Create hunxiao.cpp

---
+++ hunxiao.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+// logic
+#define rg if
+// using namespace
+#define sy using
+#define mmgs namespace
+// function
+#define js class
+#define xk void
+#define db struct
+#define fgk private
+#define gk public
+#define fh return
+// value type
+#define zs int
+#define fds float
+#define ss double
+#define wz std::string
+#define ygz char
+// unsigned signed int
+#define wqm unsigned
+#define qm signed
+#define zs8 int8_t
+#define zs16 int16_t
+#define zs32 int32_t
+#define zs64 int64_t
+#define zs8u uint8_t
+#define zs16u uint16_t
+#define zs32u uint32_t
+#define zs64u uint64_t
+// switch
+#define lg switch
+#define sj case
+#define mr default
